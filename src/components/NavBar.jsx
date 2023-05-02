08af35ad947ce9f75ba800b5ab4c0bf666dd5e31
MotorcycleEnjoyer <116019878+MotorcycleEnjoyer@users.noreply.github.com>
2023-05-02 16:56:10 -0400

    move link logic to another line for readability

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,9 @@ export default function NavBar ({ ...props }) {
                 <div className="brand">BRAND</div>
                 <div className="links">
                     {props.links.map((x, index) => {
-                        return <NavLink key={index} to={x}>{x.split("/")[1]}</NavLink>
+                        return <NavLink key={index} to={x}>
+                                    {x.split("/")[1] || "Home"}
+                                </NavLink>
                     })}
                 </div>
             </nav>
